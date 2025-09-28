dfb904627b31ae95f9c20ca06b2300bdaddcbbd6
SkyMonder <rilloperdagelo@gmail.com>
2025-09-28 14:53:15 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -80,8 +80,3 @@ app.get('/api/messages', (req, res) => {
 });
 
 app.listen(10000, () => console.log('Server running on port 10000'));
-
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
